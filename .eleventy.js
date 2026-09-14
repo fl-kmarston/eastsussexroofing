b08e95255d5c59ac5a495e2f71fd46a79b9d0de0
gen6 automation <ot-automation@gen6.local>
2026-09-14 18:01:48 +0000

    Fix GitHub Pages project paths

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addWatchTarget("src/static/");
   eleventyConfig.addFilter("dateReadable", (value) => value);
   return {
+    pathPrefix: process.env.ELEVENTY_PATH_PREFIX || "/",
     dir: { input: "src", includes: "_includes", data: "_data", output: "dist" },
     templateFormats: ["md", "njk", "html"],
     markdownTemplateEngine: "njk",
